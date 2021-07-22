0312c09ef3aba5a10d2c7e29c0e1b73333923e92
Péter Szász <eszpee@gmail.com>
2021-07-22 14:49:40 +0200

    Table for Leads

--- migrations/202107221414-CreateLeadTable.js
+++ migrations/202107221414-CreateLeadTable.js
@@ -12,7 +12,7 @@ module.exports = {
                 allowNull: false,
                 type: Sequelize.DATE
             },
-            updateAt: {
+            updatedAt: {
                 allowNull: false,
                 type: Sequelize.DATE
             },
